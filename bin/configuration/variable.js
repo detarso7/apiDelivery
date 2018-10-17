166a8a493749c10d1b91827e5a4d1036db246e6f
Saulo de Tarso <de_tarso7@hotmail.com>
2018-10-17 16:56:26 -0300

    Criação do DB e configuração da conecção com mongoose

--- bin/configuration/variable.js
+++ bin/configuration/variable.js
@@ -1,6 +1,9 @@
 const variables = {
     Api: {
         port: process.env.port || 3000
+    },
+    Database:{
+        connection: process.env.connection || 'mongodb://detarso7@132:Moscas@132@ds135233.mlab.com:35233/delivery'
     }
 }
 
--- bin/express.js
+++ bin/express.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+const variables = require('../bin/configuration/variable')
 
 //importando routers
 const categoriaRouter = require('../routes/categorias-routes')
@@ -11,6 +13,10 @@ const app = express();
 //Configuração de parse do JSON
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+//Configuração de conecção com o banco de dados
+
+mongoose.connect(variables.Database.connection)
 
 //Configurando as rotas
 app.use('/api/categorias', categoriaRouter);
