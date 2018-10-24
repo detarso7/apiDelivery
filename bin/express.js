640accc07587a3bcbe56013a50c5c9cc031a7d97
Saulo de Tarso <de_tarso7@hotmail.com>
2018-10-24 20:57:59 -0300

    Adicionando o Usuario no Arquivo express.js

--- bin/express.js
+++ bin/express.js
@@ -6,6 +6,8 @@ const variables = require('../bin/configuration/variable')
 //importando routers
 const categoriaRouter = require('../routes/categorias-routes')
 const produtosRouter = require('../routes/produtos-routes')
+const usuariosRouter = require('../routes/usuarios-routes')
+
 
 //Criando/Invocando a Api/Server Web do Express
 const app = express();
@@ -23,6 +25,8 @@ mongoose.set('useCreateIndex', true);
 //Configurando as rotas
 app.use('/api/categorias', categoriaRouter);
 app.use('/api/produtos', produtosRouter);
+app.use('/api/susarios', usuariosRouter);
+
 
 //Exportando nossa Api
 module.exports = app;
