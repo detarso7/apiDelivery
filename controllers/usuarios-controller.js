47b5449338861b4bee3a16602b5ea8d399dc039a
Saulo de Tarso <de_tarso7@hotmail.com>
2018-10-24 23:24:55 -0300

    Criando validação do uruário

--- controllers/usuarios-controller.js
+++ controllers/usuarios-controller.js
@@ -1,34 +1,67 @@
 'use strict'
 const repository = require('../repositories/usuarios-repository')
+const validation = require('../bin/helpers/validation')
+const ctrlBase = require('../bin/base/controller-base')
 const _repo = new repository()
+const md5 = require('md5')
 
 function usuariosController(){
 
 };
 
 usuariosController.prototype.post = async (req, res) => {
-    let resultado = await _repo.create(req.body)
-    res.status(201).send(resultado)
+    let _validationContract = new validation()
+
+    _validationContract.isRequired(req.body.nome, 'Informe seu nome')
+    _validationContract.isRequired(req.body.email, 'Informe seu e-mail')
+    _validationContract.isEmail(req.body.email, 'O e-mail informado é inválido')
+    _validationContract.isRequired(req.body.senha, 'Informe sua senha')
+    _validationContract.isRequired(req.body.senhaConfirmacao, 'A senha de confirmação é obrigatória')
+    _validationContract.isTrue(req.body.senha != req.body.senhaConfirmacao, 'A senha de confirmação deve ser igual a senha informada')
+
+    let usuarioIsEmailExiste = await _repo.isEmailExiste(req.body.email)
+    if(usuarioIsEmailExiste){
+        _validationContract.isTrue(usuarioIsEmailExiste != undefined, `Já existe o email ${req.body.email} em nosso cadastro.`)
+    }
+
+    req.body.senha = md5(req.body.senha)
+
+    ctrlBase.post(_repo, _validationContract, req, res)
+
 };
 
 usuariosController.prototype.put = async (req, res) => {
-    let resultado = await _repo.updata(req.params.id, req.body)
-    res.status(202).send(resultado)
+
+    let _validationContract = new validation()
+
+    _validationContract.isRequired(req.body.nome, 'Informe seu nome')
+    _validationContract.isRequired(req.body.email, 'Informe seu e-mail')
+    _validationContract.isEmail(req.body.email, 'O e-mail informado é inválido')
+    _validationContract.isRequired(req.params.id, 'Informe o ID do usuário')
+
+    let usuarioIsEmailExiste = await _repo.isEmailExiste(req.body.email)
+    if(usuarioIsEmailExiste){
+        _validationContract.isTrue(
+            usuarioIsEmailExiste.nome != undefined &&
+            usuarioIsEmailExiste._id != undefined,
+            `Já existe o email ${req.body.email} em nosso cadastro.`)
+    }
+
+    ctrlBase.put(_repo, _validationContract, req, res)
 };
 
 usuariosController.prototype.get = async (req, res) => {
-    let lista = await _repo.getAll()
-    res.status(200).send(lista)
+ctrlBase.get(_repo, _validationContract, req, res)
 };
 
 usuariosController.prototype.getById = async (req, res) => {
-    let categoria = await _repo.getById(req.params.id)
-    res.status(200).send(categoria)
+    ctrlBase.getById(_repo, _validationContract, req, res)
+
 };
 
 usuariosController.prototype.delete = async (req, res) => {
-    let deletado = await _repo.delete(req.params.id)
-    res.status(204).send(deletado)
+    ctrlBase.delete(_repo, _validationContract, req, res)
+
 };
 
 module.exports = categoriaController
--- repositories/usuarios-repository.js
+++ repositories/usuarios-repository.js
@@ -2,26 +2,43 @@
 
 require('../model/usuarios-model')
 const base = require('../bin/base/repository-base')
+const md5 = require('md5')
 
 class UsuariosRepositorio{
     constructor(){
         this._base = new base('Usuarios')
+        this.projection = 'nome email _id'
+    }
+
+    async isEmailExiste(Email){
+        return await this._base._model.findOne({email: Email}, this.projection)
+    }
+
+    async authentication(Email, Senha){
+        let _hashSenha = md5(Senha)
+        return await this._base._model.findOne({email: Email, senha: _hashSenha}, this.projection)
     }
 
     async create(data){
-        return await this._base.create(data)        
+        let usuarioCriado = await this._base.create(data)
+        return this._base._model.findById(usuarioCriado._id, this.projection)
     }
 
     async updata(id, data){
-        return  await this._base.updata(id, data);
+        let usuarioAtualizado = await this._base.updata(id, {
+            nome: data.nome,
+            email: data.email,
+            foto: data.foto
+        })
+        return  this._base._model.findById(usuarioAtualizado._id, this.projection)
     }
 
     async getAll(){
-        return await this._base.getAll()
+        return await this._base._model.find({}, this.projection)
     }
 
     async getById(id){
-        return await this._base.getById(id)
+        return await this._base._model.findById(id, 'nome email _id foto')
     }
 
     async delete(id){
